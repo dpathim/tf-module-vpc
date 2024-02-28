847fdb8a5455cc1c503c02373567a568c6e9d4e9
Deepthi <dpdevops6@gmail.com>
2024-02-28 17:46:09 -0500

    Terraform

--- output.tf
+++ output.tf
@@ -1,3 +1,12 @@
-output "subnet" {
+output "subnets" {
   value = module.subnets
+}
+
+output "vpc_id" {
+  value = "aws_vpc.main.id"
+
+}
+
+output "public_subnet_ids" {
+  value = "local.public_subnet_ids"
 }
