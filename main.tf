1dffe27eacee4d2be605d0b249505e1ee87e10c7
Deepthi <dpdevops6@gmail.com>
2024-02-27 19:34:48 -0500

    Terraform

--- main.tf
+++ main.tf
@@ -26,16 +26,29 @@ resource "aws_route" "igw" {
 }
 
 resource "aws_eip" "ngw" {
-  for_each = lookup(lookup(module.subnets, "public", null), "subnet_ids", null)
+  for_each = local.public_subnet_ids
   domain   = "vpc"
 }
 resource "aws_nat_gateway" "ngw" {
-  for_each = lookup(lookup(module.subnets, "public", null), "subnet_ids", null)
+  count = local.public_subnet_ids
+  allocation_id = element(aws_eip.ngw.*.id, count.index)
+    subnet_id    = element(local.public_subnet_ids, count.index)
+      }
 
-  allocation_id = lookup(lookup(aws_eip.ngw, each.key, null), "id", null)
-  subnet_id     = each.value["id"]
 
+  resource "aws_route" "ngw" {
+    count        = local.private_route_table_ids
+    route_table_id            = element(local.private_route_table_ids, count.index)
+    destination_cidr_block    = "0.0.0.0/0"
+    nat_gateway_id                = element(aws_nat_gateway.ngw.*id,count.index)
   }
+
+
+
+
+
+
+
 output "subnet" {
   value = module.subnets
 }
