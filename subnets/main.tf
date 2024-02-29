150de345ffede458385473498dd9dea60384f691
Deepthi <dpdevops6@gmail.com>
2024-02-29 18:37:29 -0500

    Terraform

--- subnets/main.tf
+++ subnets/main.tf
@@ -1,13 +1,14 @@
 resource "aws_subnet" "main" {
-  for_each = var.subnets
-  vpc_id     = var.vpc_id
-  cidr_block = each.value["cidr"]
+  for_each          = var.subnets
+  vpc_id            = var.vpc_id
+  cidr_block        = each.value["cidr"]
   availability_zone = each.value["az"]
-  tags = {
-    name = each.key
-  } #merge(var.tags, { Name = "${var.env}-${each.key}-subnet"})
-#
+  tags              = {
+    Name = each.key
+  }
 }
+##merge(var.tags, { Name = "${var.env}-${each.key}-subnet"})
+
 #
 #resource "aws_route_table" "main" {
 #
