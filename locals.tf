946af69e64ec35d0ce483e7f07789a0c7aab1a97
Deepthi <dpdevops6@gmail.com>
2024-02-29 15:24:47 -0500

    Terraform

--- locals.tf
+++ locals.tf
@@ -7,7 +7,7 @@ locals {
   private_subnet_ids = concat(local.app_subnet_ids, local.db_subnet_ids)
 
   #RT
-  public_rout_table_ids = [for k,v in lookup(lookup(module.subnets, "public", null), "route_table_ids", null) : v.id]
+  public_rout_table_ids = [for k,v in lookup(lookup(module.subnets, "public", null), "aws_route_table", null) : v.id]
   app_rout_table_ids = [for k,v in lookup(lookup(module.subnets, "app", null), "aws_route_table", null) : v.id]
   db_rout_table_ids = [for k,v in lookup(lookup(module.subnets, "db", null), "aws_route_table", null) : v.id]
   private_rout_table_ids = concat(local.app_rout_table_ids, local.db_rout_table_ids)
