bbc8868e3949df4b7edbaa019fbc5d30b24c0d28
Deepthi <dpdevops6@gmail.com>
2024-03-01 13:42:55 -0500

    Terraform

--- output.tf
+++ output.tf
@@ -1,6 +1,6 @@
-#output "subnets" {
-#  value = module.subnets
-#}
+output "subnets" {
+  value = module.subnets
+}
 #
 #output "public_subnet_ids" {
 #  value = "local.public_subnet_ids"
--- subnets/outputs.tf
+++ subnets/outputs.tf
@@ -1,7 +1,7 @@
-#output "subnet_ids" {
-#  value = aws_subnet.main
-#}
-#
-#output "aws_route_table_ids" {
-#  value = aws_route_table.main
-#}
+output "subnet_ids" {
+  value = aws_subnet.main
+}
+
+output "route_table_ids" {
+  value = aws_route_table.main
+}
