3116ab503414bc0d33a526525e8ac65b37704de1
Deepthi <dpdevops6@gmail.com>
2024-03-01 14:28:46 -0500

    Terraform

--- main.tf
+++ main.tf
@@ -32,22 +32,33 @@ resource "aws_route" "igw" {
 #  count = length(local.public_subnet_ids)
 #  domain   = "vpc"
 #}
+
+resource "aws_eip" "ngw" {
+  for_each  = lookup(lookup(module.subnets, "public", null), "subnets_ids", null)
+  domain   = "vpc"
+}
+
+resource "aws_nat_gateway" "ngw" {
+  for_each  = lookup(lookup(module.subnets, "public", null), "subnets_ids", null)
+  allocation_id = lookup(lookup(aws_eip.ngw, each.key, null), "ids", null)
+  subnet_id     = each.value["ids"]
+}
 #resource "aws_nat_gateway" "ngw" {
 #  count = length(local.public_subnet_ids)
 #  allocation_id = element(aws_eip.ngw.*.id, count.index)
 #    subnet_id    = element(local.public_subnet_ids, count.index)
 #  tags = merge(local.tags, { Name = "${var.env}-ngw"})
 #      }
-#
-#
+
+
 #  resource "aws_route" "ngw" {
 #    count        = length(local.private_rout_table_ids)
 #    route_table_id            = element(local.private_rout_table_ids, count.index)
 #    destination_cidr_block    = "0.0.0.0/0"
 #    nat_gateway_id                = element(aws_nat_gateway.ngw.*.id, count.index)
 #  }
-#
-#
+
+
 #resource "aws_vpc_peering_connection" "peering" {
 #  peer_vpc_id   = aws_vpc.main.id
 #  vpc_id        = var.default_vpc_id
