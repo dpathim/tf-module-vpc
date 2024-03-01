889cd9b3ccd9ad0647ce31b0d599326b9663ba13
Deepthi <dpdevops6@gmail.com>
2024-03-01 16:12:16 -0500

    Terraform

--- main.tf
+++ main.tf
@@ -92,7 +92,7 @@ resource "aws_security_group" "allow_tls" {
 }
 
 resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
-
+  security_group_id = aws_security_group.allow_tls.id
   cidr_ipv4        = "0.0.0.0/0"
   from_port         = 22
   ip_protocol       = "tcp"
@@ -102,13 +102,13 @@ resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
 
 
 resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
-
+  security_group_id = aws_security_group.allow_tls.id
   cidr_ipv4         = "0.0.0.0/0"
   ip_protocol       = "-1" # semantically equivalent to all ports
 }
 
 resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv6" {
-
+  security_group_id = aws_security_group.allow_tls.id
   cidr_ipv6         = "::/0"
   ip_protocol       = "-1" # semantically equivalent to all ports
 }
